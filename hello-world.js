d0c387d3fabdf407a5af068e25862aeb1789cbbf
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-18 17:40:28 +0000

    ex1 - hello world with timeout

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+// First program
+// -----------------------------------------
+
+// Create a file called hello-world.js . In it, write a simple node program that outputs "Hello World!" to the console.
+// Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the program was run.
+
+setTimeout(function() {
+    console.log("Hello World Again!!");
+}, 10000);
+console.log("Hello World");
